208cb59d7fc072e8eac6aca927db9b8700d60291
Łukasz Mieczkowski <mieczkowski.lukasz@gmail.com>
2020-08-25 21:15:26 +0200

    [my-first-app] Keep SECRET_KEY in env variable

--- my-first-app/threelevels/threelevels/settings.py
+++ my-first-app/threelevels/threelevels/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '__))r7ax$&ov=l&ta-0k6!vumnx1z-3%jx66=(2)t*e!$a3qjt'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
